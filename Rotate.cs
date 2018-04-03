07dd458e3f8fa5cafee73be659bc170b041c7844
xuyi26 <31821345+xuyi26@users.noreply.github.com>
2018-04-03 19:47:38 +0800

    Add files via upload

---
+++ Rotate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+    public Transform origin;
+    public float speed = 10f;
+    public float speed_ = 2f;
+    float y, z;
+	void Start () {
+        y = Random.Range(1, 360);
+        z = Random.Range(1, 360);
+
+    }
+	
+	
+	void Update () {
+        Vector3 axis = new Vector3(0, y, z);
+        this.transform.RotateAround(origin.position, axis, speed * Time.deltaTime);
+        this.transform.Rotate(Vector3.up * speed_ * Time.deltaTime);
+	}
+}
