73454190b8928f4bf29c0703a5f4d24297cbc8ca
xuyi26 <31821345+xuyi26@users.noreply.github.com>
2018-04-03 23:05:00 +0800

    Add files via upload

---
+++ PriestsAndDevils/Controllor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using myGame;
+
+public class Controllor : MonoBehaviour ,IScenceController,IUserAction{
+    
+}
+
+public class Director : System.Object
+{
+    private static Director _instance;
+    public ISceneController currentSceneController { get; set; }
+    public static Director getInstance()
+    {
+        if (_instance == null)
+        {
+            _instance = new Director();
+        }
+        return _instance;
+    }
+}
+public interface ISceneController
+{
+    void loadResources();
+}
+public class MyCharacterController {
+
+}
+public class ShoreController{
+}
+public class BoatController{
+    
+}
---
+++ PriestsAndDevils/UserGUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using mygame;
+public class UserGUI : MonoBehaviour {
+    private IUserAction ac
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+public interface UserAction
+{
+    void moveBoat();
+    void restart();
+}
+public class ClickGUI : MonoBehaviour
+{
+    UserAction action;
+    MyCharacterController characterController;
+    public void setController(MyCharacterController characterCtrl)
+    {
+        chara
+    }
+}
